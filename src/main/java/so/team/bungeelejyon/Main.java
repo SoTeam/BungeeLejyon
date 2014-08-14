74c9a712bbddac9ae03be56dc941e71ad7dbbf5b
soteam <specialgaming85@gmail.com>
2014-08-15 02:21:12 +0300

    Başlıyoruz :P

---
+++ src/main/java/so/team/bungeelejyon/Main.java
@@ -0,0 +1,12 @@
+package so.team.bungeelejyon;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+	
+    @Override
+    public void onEnable() {
+        getLogger().info("Basliyoruz :P");
+    }
+    
+}
